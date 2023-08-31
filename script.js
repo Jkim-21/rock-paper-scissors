db664ba7668800f7c870de4828a385980449311a
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2023-08-30 20:50:01 -0400

    Create function to play round of rock paper scissors
    The function outputs different results depending on
    the choice of the computer and user.

--- script.js
+++ script.js
@@ -2,8 +2,61 @@
 // Name: Jonathan Kim
 
 function getComputerChoice() {
-    let computerChoices = ["Rock", "Paper", "Scissors"];
+    let computerChoices = ["rock", "paper", "scissors"];
     let choiceNumber = Math.floor(Math.random() * computerChoices.length);
-    return computerChoices(choiceNumber);
+    return computerChoices[choiceNumber];
 }
 
+function playRound(playerSelection, computerSelection) {
+    let userAnswer = playerSelection.toLowerCase();
+    
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            return ("You draw! Rock ties with Rock")
+        }
+        if (computerSelection == "scissors") {
+            return ("You win! Rock beats Scissors")
+        }
+        if (computerSelection == "paper") {
+            return ("You lose! Rock loses against Paper")
+        }
+    }
+
+    if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            return ("You lose! Scissors loses against Rock")
+        }
+        if (computerSelection == "scissors") {
+            return ("You tie! Scissors ties with Scissors")
+        }
+        if (computerSelection == "paper") {
+            return ("You win! Scissors beats Paper")
+        }
+    }
+
+    if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return ("You win! Paper beats Rock")
+        }
+        if (computerSelection == "scissors") {
+            return ("You lose! Paper loses against Scissors")
+        }
+        if (computerSelection == "paper") {
+            return ("You tie! Paper ties with Paper")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let rawInput = prompt("Let's play Rock, Paper, Scissors. Enter one of the three options in the game.", "");
